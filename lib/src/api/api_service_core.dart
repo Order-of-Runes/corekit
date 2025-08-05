235e181a8061e9f6aca7970ea648d26da51d3dd4
ishowrunes <ishworisgod@gmail.com>
2025-08-05 09:50:43 +0545

    Add core for api service and it's helping abstractions

---
+++ lib/src/api/api_service_core.dart
@@ -0,0 +1,192 @@
+// Copyright (c) 2024 EShare Authors. All rights reserved.
+
+import 'package:corekit/src/foundation/api_url_foundation.dart';
+import 'package:corekit/src/foundation/url_prefix_foundation.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:foundation/foundation.dart';
+import 'package:rusty_dart/rusty_dart.dart';
+import 'package:utils/utils.dart';
+
+typedef OnApiError = FailureFoundation Function(Map<String, dynamic>);
+
+abstract class ApiServiceCore {
+  ApiServiceCore({
+    required String baseUrl,
+    List<Interceptor>? interceptors,
+  }) {
+    const timeout = Duration(minutes: 1);
+    const receiveTimeout = Duration(minutes: 5);
+    final baseOptions = BaseOptions(
+      baseUrl: baseUrl,
+      receiveTimeout: timeout,
+      connectTimeout: receiveTimeout,
+      validateStatus: (status) => status != null && status >= 200 && status < 300,
+    );
+    _dio = Dio();
+
+    if (interceptors.isNotNullAndNotEmpty) {
+      _dio.interceptors.addAll(interceptors!);
+    }
+
+    _dio.options = baseOptions;
+    cancelToken ??= CancelToken();
+  }
+
+  CancelToken? cancelToken;
+
+  String get baseUrl => _dio.options.baseUrl;
+
+  set baseUrl(String url) => _dio.options.baseUrl = url;
+
+  late Dio _dio;
+
+  /// To set independent scenarios for different api
+  final Map<ApiUrlFoundation, String> _testScenarios = {};
+
+  /// This field is exclusively for tests
+  ///
+  /// To set independent scenarios for different api
+  ///
+  /// In Mockoon, provide a header with key `s` and value from
+  @visibleForTesting
+  void setScenario({required ApiUrlFoundation url, required String scenario}) {
+    _testScenarios[url] = scenario;
+  }
+
+  Future<Result<Object?, FailureFoundation>> request<T>({
+    required RestMethod method,
+    required ApiUrlFoundation url,
+    required Options options,
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Map<String, String>? pathParams,
+    UrlPrefixFoundation? prefix,
+    ProgressCallback? onSendProgress,
+    bool isTest = false,
+    OnApiError? onError,
+  }) async {
+    if (cancelToken?.isCancelled ?? false) cancelToken = CancelToken();
+
+    final path = _updatePath(url, pathParams, prefix);
+    try {
+      final Options _options;
+      if (isTest) {
+        final scenario = isTest ? _testScenarios[url] : null;
+        _testScenarios.remove(url);
+        _options = scenario.isNullOrEmpty
+            ? options
+            : options.copyWith(
+                headers: {
+                  if (options.headers.isNotNullAndNotEmpty) ...options.headers!,
+                  's': scenario,
+                },
+              );
+      } else {
+        _options = options;
+      }
+
+      final _response = await _dio.request<Object>(
+        path,
+        data: data,
+        options: _options,
+        queryParameters: queryParameters,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+      );
+
+      return Ok(_response.data);
+    } on DioException catch (e, s) {
+      return Err(_mapDioException(e, s, onError));
+    }
+  }
+
+  FailureFoundation _mapDioException(
+    DioException exception,
+    StackTrace stackTrace,
+    OnApiError? onError,
+  ) {
+    switch (exception.type) {
+      case DioExceptionType.connectionTimeout:
+      case DioExceptionType.sendTimeout:
+      case DioExceptionType.receiveTimeout:
+        return const FailureFoundation(
+          'Connection Timeout',
+          source: 'connectionTimeout',
+          detail: 'There was a problem while completing your request. Please try again later.',
+        );
+      case DioExceptionType.badResponse:
+        final statusCode = exception.response?.statusCode;
+        if (statusCode == 500 || statusCode == 503) {
+          return FailureFoundation(
+            'Internal server error',
+            detail: 'The server encountered an error & was unable to complete your request. Please try again later.',
+            source: 'api',
+            code: double.tryParse(statusCode?.toString() ?? '500'),
+          );
+        }
+
+        final data = exception.response?.data;
+        if (data == null || (statusCode == 404 && data == null)) {
+          return const FailureFoundation(
+            'No Data',
+            detail: _genericErrorMessage,
+            code: 404,
+            source: 'api',
+          );
+        }
+
+        if (data is String) {
+          return FailureFoundation(
+            exception.message ?? '',
+            detail: data,
+            source: 'api',
+          );
+        }
+
+        if (data is Map<String, dynamic> && onError.isNotNull) return onError!(data);
+
+        return const FailureFoundation(_genericErrorMessage, source: 'api');
+
+      case DioExceptionType.cancel:
+        return const FailureFoundation('Request Cancelled', code: 100, source: 'cancel');
+      case DioExceptionType.badCertificate:
+      case DioExceptionType.connectionError:
+      case DioExceptionType.unknown:
+        final _error = exception.error;
+
+        return FailureFoundation(
+          _error.toString(),
+          detail: _error.toString(),
+          stackTrace: stackTrace,
+          source: 'network',
+        );
+    }
+  }
+
+  String _updatePath(
+    ApiUrlFoundation url,
+    Map<String, String>? pathParams,
+    UrlPrefixFoundation? prefix,
+  ) {
+    final updatedPath = pathParams.isNullOrEmpty
+        ? url.path
+        : url.path.fillInUrl(
+            keyValues: pathParams!,
+          );
+
+    final resolvedPrefix = prefix.isNull ? '' : '${prefix!.path}/';
+
+    return Uri.parse(updatedPath).hasScheme ? updatedPath : '$resolvedPrefix$updatedPath';
+  }
+}
+
+enum RestMethod {
+  get,
+  post,
+  put,
+  delete,
+  patch,
+}
+
+const _genericErrorMessage = 'We were unable to complete your request. Please try again later.';
