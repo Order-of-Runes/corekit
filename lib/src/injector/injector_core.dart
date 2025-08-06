fa4c54b10aaa6259584a65387e8df4c18c335125
ishowrunes <ishworisgod@gmail.com>
2025-08-06 14:00:08 +0545

    Add more injectables

--- lib/src/injector/injector_core.dart
+++ lib/src/injector/injector_core.dart
@@ -1,7 +1,9 @@
 // Copyright (c) 2025 Order of Runes Authors. All rights reserved.
 
 import 'package:corekit/src/api/api_service_core.dart';
+import 'package:corekit/src/api/interceptors/dio_memory_logger.dart';
 import 'package:corekit/src/base/base_network_util.dart';
+import 'package:corekit/src/modals/dialog_hub.dart';
 import 'package:corekit/src/router/router.dart';
 import 'package:corekit/src/store/store.dart';
 
@@ -17,4 +19,8 @@ abstract class InjectorCore {
   BaseNetworkUtil get network;
 
   RouterCore get router;
+
+  DialogHubCore get dialogHub;
+
+  DioMemoryLogger get dioMemoryLogger;
 }
