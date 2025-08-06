54b1da33ab5f3e81c127aa6ac11040303a4e5465
ishowrunes <ishworisgod@gmail.com>
2025-08-06 14:04:18 +0545

    Add requestPermissionCore to handle requesting of runtime permissions

---
+++ lib/src/permission/permission.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:foundation/foundation.dart';
+import 'package:rusty_dart/rusty_dart.dart';
+
+/// Requests the permission.
+Future<void> requestPermissionCore({
+  required BuildContext context,
+  required Future<Result<bool, FailureFoundation>> Function() onPermission,
+  required VoidCallback onGranted,
+  required String title,
+  required String description,
+  VoidCallback? onDenied,
+  ValueChanged<FailureFoundation>? onError,
+}) async {
+  final permissionResult = await onPermission();
+
+  permissionResult.match(
+    ok: (status) {
+      if (status) return onGranted();
+
+      onDenied?.call();
+    },
+    err: (failure) => onError?.call(failure),
+  );
+}
